47b40380495f2f3249c10041777d1e76367214f0
devfloors <csh47790@gmail.com>
2022-12-30 00:03:30 +0900

    user domain entity 추가

---
+++ src/main/java/com/boilerplate/user/domain/Authority.java
@@ -0,0 +1,2 @@
+package com.boilerplate.user.domain;public class Authority {
+}
---
+++ src/main/java/com/boilerplate/user/domain/User.java
@@ -0,0 +1,53 @@
+package com.boilerplate.user.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.BatchSize;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Data
+public class User implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+//    @Size(min = 1,max = 50)
+    @Column(name = "password", length = 60, nullable = false)
+    private String password;
+
+//    @Size(max = 50)
+    @Column(name = "first_name", length = 50)
+    private String firstName;
+
+//    @Size(max = 50)
+    @Column(name = "last_name", length = 50)
+    private String lastName;
+
+//    @Email
+//    @Size(min = 5, max = 254)
+    @Column(length = 254, unique = true)
+    private String email;
+
+    @JsonIgnore
+    @ManyToMany
+    @JoinTable(
+            name = "user_authority",
+            joinColumns = {@JoinColumn(name = "user_id", referencedColumnName = "id")},
+            inverseJoinColumns = {@JoinColumn(name = "authority_name",referencedColumnName = "name")}
+    )
+    @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+    @BatchSize(size = 20)
+    private Set<Authority> authorities = new HashSet<>();
+}
