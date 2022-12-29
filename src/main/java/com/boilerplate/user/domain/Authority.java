f8a7f4ed05bf15f9d9baebdfa2139bd3cb9062e9
devfloors <csh47790@gmail.com>
2022-12-30 00:03:50 +0900

    authority domain entity 추가

--- src/main/java/com/boilerplate/user/domain/Authority.java
+++ src/main/java/com/boilerplate/user/domain/Authority.java
@@ -1,2 +1,16 @@
-package com.boilerplate.user.domain;public class Authority {
+package com.boilerplate.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.Serializable;
+
+public class Authority implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @NotNull
+    @Id
+    @Column(length = 50)
+    private String name;
 }
