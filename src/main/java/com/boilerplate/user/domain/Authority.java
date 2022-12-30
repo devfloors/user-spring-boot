a82f914ce7f47f4b27101455e113359f4107ac29
devfloors <csh47790@gmail.com>
2022-12-30 22:14:52 +0900

    docker-compose 추가

--- src/main/java/com/boilerplate/user/domain/Authority.java
+++ src/main/java/com/boilerplate/user/domain/Authority.java
@@ -1,11 +1,22 @@
 package com.boilerplate.user.domain;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.Serializable;
 
+@Entity
+@Table(name = "authority")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+@Data
+@NoArgsConstructor
 public class Authority implements Serializable {
     private static final long serialVersionUID = 1L;
 
